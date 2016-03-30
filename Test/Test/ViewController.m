239e47533726e4f5a28e60ee44653043e1945ea4
Ibokan <1261308886@qq.com>
2016-03-30 12:26:35 +0800

    测试

---
+++ Test/Test/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by Ibokan on 16/3/30.
+//  Copyright © 2016年 ikbon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"test");
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
